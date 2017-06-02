cd41f99c2b77fd0c99ca755b28ba6edfd70d9a4d
clara <clara@clara.com>
2017-06-02 11:03:41 -0500

    Class checklist

--- src/main/java/week_1/Question_5_Class_Setup.java
+++ src/main/java/week_1/Question_5_Class_Setup.java
@@ -2,45 +2,156 @@ package week_1;
 
 /**
 *
+ * Class setup checklist
  *
+ * This isn't really a program, but read through the checklist, complete the items, and update
+ * the variables as you complete each task so the autograder knows you've done all the things.
+ *
+ * Any questions or problems with any of these things? Please get in touch!
  *
  * */
-public class Question_5_Class_Setup {
+
+
+ public class Question_5_Class_Setup {
 
     /*
-    * . Reading:
+    1. Reading:
+        Read Chapter 1, Java Fundamentals, and Chapter 2, Introducing Data Types and Operators.
 
-Read Chapter 1, Java Fundamentals, and Chapter 2, Introducing Data Types and Operators.
+        Set this boolean to true once you've completed this
+    */
 
-2. Question. What was the most confusing part of Chapter 1?
+    public boolean haveDoneReading = false;
 
-3. Question. What was the most confusing part of Chapter 2?
 
-4. Complete all of the following:
 
-1. Install Java 8 JDK on your own computer, or verify how to use it on the ITEC lab computers.
+    /* Question. What was the most confusing part of Chapter 1?
 
-2. Install IntelliJ Ultimate Edition on your own computer, or verify how to use it on the ITEC lab computers.
+        Replace the empty string with your answer
+     */
 
-3. Verify you can create and run a Hello World program on your own computer, or the ITEC computers.
+    public String mostConfusingPartOfChapter1 = "";
 
-4. Read the entire syllabus
 
-5. Plan a 2-3 hour block of time per week that you will use to work on the Advanced labs. If you are using our computers, make sure you know what our opening hours are.
 
-6. Decide on times that you will use to work on labs, programming practice, projects, and other class materials each week. If you are using our computers, make sure you know what our opening hours are.
+    /* Question. What was the most confusing part of Chapter 2?
 
-7. Make sure you understand what the requirements for the labs are. What is due when? What do you need to upload?
+        Replace the empty string with your answer
+     */
 
-8. Make sure you know when open lab/office hours are
+    public String mostConfusingPartOfChapter2 = "";
 
-9. Make sure you have the textbook.
 
-10. Email me ASAP if you have any questions on anything to do with the class!
 
-11. Type a message below confirming that you've done all of the above.
+    /*  (If using your own computer)
+    *    Installing software:  Install Java 8 JDK on your own computer
+    *
+    *    Set this boolean to true once you've completed this
+    */
 
-View as PageDownloadToggle Fullscreen
-DownloadPrevious  Next
-*/
+    public boolean haveInstalledJava8 = false;
+
+
+
+    /*   (If using your own computer and you have a PC)
+    *    Installing software:  Install git on your own computer
+    *    https://git-scm.com/
+    *
+    *    Mac and Linux users: you should already have git. Type git --version at a terminal to verify.
+    *
+    *    Set this boolean to true once you've completed this
+    */
+
+    public boolean haveInstalleOrConfirmedInstallOfdGit = false;
+
+
+    /* Install IntelliJ ***Ultimate*** Edition on your own computer,
+    *  or verify you can use it on the ITEC lab computers.
+    *    Set these boolean to true once you've completed this
+     */
+
+    public boolean haveInstalledIntelliJ = false;
+    public boolean installedUltimateEdition = false;
+
+
+
+    /* Verify you can create and run a Hello World java program on your own computer,
+    * or the ITEC computers.
+    *
+    *    Set this boolean to true once you've completed this
+    */
+
+    public boolean canRunHelloWorldProgram = false;
+
+
+
+    /*  Read the entire syllabus
+    * Contact me ASAP if you have any question on the syllabus or class schedule.
+    *
+    *    Set this boolean to true once you've completed this
+    */
+
+    public boolean haveReadEntireSyllabus = false;
+    public boolean contactedClaraWithAnyQuestionsOnSyllabus = false;
+
+
+
+    /*  Plan block(s) of time per week that you will use to work on the class materials; labs and projects
+    *  You will need around 12 hours per week to work on this class
+    *  This is a 6-credit class and typically students will need to spend 2x hours outside of class
+    *  If you are using our computers, make sure you know what our opening hours are.
+    *
+    *  Set this boolean to true once you've completed this
+    */
+
+    public boolean havePlannedWhenToWorkOnClass = false;
+
+
+    /* Labs
+
+    * Make sure you have the labs set up. You should have downloaded the lab repository
+    * You should have your own GitHub account
+    * You should know how to commit your changes and push them to your own GitHub account
+    *
+    * Make sure you understand how to find each week's lab work
+    * Make sure you have the schedule for lab deadlines
+    *
+    * Set these boolean to true once you've completed this
+    */
+
+    public boolean understandHowToDoLabs = false;
+    public boolean haveLabRepoAndGitHubSetUpCorrectly = false;
+
+
+
+    /*  Make sure you know when open lab/office hours are
+     *    Set this boolean to true once you've completed this
+    */
+
+    public boolean knowWhenOpenLabIs = false;
+
+
+
+    /*  Make sure you have the textbook.
+      *    Set this boolean to true once you've completed this
+    */
+    public boolean haveTextbook = false;
+
+
+
+    /* This one is important.
+    * If you get stuck on something, or don't understand what's going on with something,
+    * or git/GitHub or IntelliJ isn't working, you should ask for help ASAP.
+
+    * Plan to email me ASAP, or ask in class/open lab if you have any questions on anything to do with the class
+    * Questions are encouraged.
+    * There's a strong correlation between number of questions asked and good grades
+    * And remember that I'm here to help you.
+
+    *    Set this boolean to true once you've completed this
+    */
+
+    public boolean willAskForHelpIfNeeded = false;
+
+
 }
